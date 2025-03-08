0bfc908c6c0f04bc2793701553e11e6b39413e61
AjeebKunhuty <137520776+AjeebKunhuty@users.noreply.github.com>
2025-03-08 11:56:01 +0530

    Add files via upload

--- src/main/java/com/HeiseiChain/HeiseiChain/HeiseiChain.java
+++ src/main/java/com/HeiseiChain/HeiseiChain/HeiseiChain.java
@@ -2,8 +2,11 @@ package com.HeiseiChain.HeiseiChain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.HeiseiChain.HeiseiChain")
+
 public class HeiseiChain {
 	public static void main(String[] args) {
 		SpringApplication.run(HeiseiChain.class, args);
